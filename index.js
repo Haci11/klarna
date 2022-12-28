6f3ad55e42ca97a1cd7550bab5e859cf4c12b6df
Haci11 <46707887+Haci11@users.noreply.github.com>
2022-12-28 11:15:34 +0100

    Added product route

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 const app = express();
+import { createOrder } from "./klarna.js";
+import { config } from "dotenv";
+config();
 
 const products = [
-  { id: 1, price: 57, name: "house" },
-  { id: 2, price: 567, name: "bob" },
-  { id: 3, price: 7, name: "toy" },
+  { id: "1", price: 57, name: "house" },
+  { id: "2", price: 567, name: "bob" },
+  { id: "3", price: 7, name: "toy" },
 ];
 
 app.get("/", (req, res) => {
@@ -13,6 +16,13 @@ app.get("/", (req, res) => {
       .map((product) => `<a href="/p/${product.id}">${product.name}</a>`)
       .join("")
   );
+});
+
+app.get("/p/:id", async (req, res) => {
+  const product = products.find((product) => product.id === req.params.id);
+  const data = await createOrder(product);
+  console.log(data);
+  res.send(data.html_snippet);
 });
 
 app.listen(3000);
